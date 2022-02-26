bd270b8fc88c5bd3160b1e354b0eb006b7763d63
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-02-26 22:22:42 +0800

    palindrome using stack

---
+++ stack/palindrome_stack.py
@@ -0,0 +1,22 @@
+import stack
+
+s = stack.Stack()
+
+text = input("Please input a palindrome: ")
+# convert to lowercase
+text = text.lower()
+
+# push each char into stack
+for each_char in text:
+    s.push(each_char)
+
+# reverse each_char in the stack
+reverse_str = ""
+
+while not s.is_empty():
+    reverse_str = reverse_str + s.pop()
+
+if text == reverse_str:
+    print("Palindrome found!")
+else:
+    print("Sorry, this word is not a palindrome :(")
